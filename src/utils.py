f6e976dd5efb9651b8da302fe21c110460351420
Larissa Silva de Morais batista <labatista@deloitte.com>
2025-11-25 11:53:27 -0300

    feat: função para valores nulos no arquivo utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,14 @@
-#retorna a consulta sql para ser utilizada no sistema
-import pandas as pd
-import yaml
-import psycopg2 #biblioteca para conectar ao banco de dados postgresql
+#funções utilitárias 
 
+import pandas as pd #biblioteca para manipulação de dados
+import yaml #biblioteca para manipulação de arquivos yaml
+import psycopg2 #biblioteca para conectar ao banco de dados postgresql
+from fuzzywuzzy import process #biblioteca para correspondência de strings
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import joblib #biblioteca para salvar e carregar modelos treinados
+import const #importa o arquivo de constantes
+
+#função para obter dados do banco de dados
 def get_data_from_db(sql_query): 
     try:
         with open('config.yaml', 'r') as file: 
@@ -28,3 +34,20 @@ def get_data_from_db(sql_query):
             con.close()
 
     return df
+
+#função para substituir valores nulos
+def substitui_nulos(df):
+    for coluna in df.columns: 
+        if df[coluna].dtype == 'object': #verifica se a coluna tem dados categóricos
+            moda = df[coluna].mode()[0] #calcula a moda da coluna
+            df[coluna].fillna(moda, inplace=True) #substitui os valores nulos pela moda
+        else:
+            mediana = df[coluna].median() #calcula a mediana da coluna
+            df[coluna].fillna(mediana, inplace=True)
+            
+
+
+
+
+            
+
